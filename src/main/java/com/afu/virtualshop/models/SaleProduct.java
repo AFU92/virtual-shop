5d1fdd4e27b7a951b5d38a4fb73e5059e9de6cfd
Andrea Fuentes <mfuentescastellanos@gmail.com>
2020-01-23 10:25:25 -0500

    add lombok dependency

--- src/main/java/com/afu/virtualshop/models/SaleProduct.java
+++ src/main/java/com/afu/virtualshop/models/SaleProduct.java
@@ -1,5 +1,7 @@
 package com.afu.virtualshop.models;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
@@ -10,6 +12,7 @@ import javax.validation.constraints.NotEmpty;
  */
 @Entity
 @Table(name = "sale_product")
+@Data
 public class SaleProduct extends AuditEntity {
 
     @Id
@@ -39,130 +42,4 @@ public class SaleProduct extends AuditEntity {
 
     @OneToOne(mappedBy = "saleProduct")
     private SaleRefundedProduct saleRefundedProduct;
-
-    /**
-     * Gets id.
-     *
-     * @return the id
-     */
-    public Integer getId() {
-        return id;
-    }
-
-    /**
-     * Sets id.
-     *
-     * @param id the id
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * Gets unit price.
-     *
-     * @return the unit price
-     */
-    public String getUnitPrice() {
-        return unitPrice;
-    }
-
-    /**
-     * Sets unit price.
-     *
-     * @param unitPrice the unit price
-     */
-    public void setUnitPrice(String unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    /**
-     * Gets quantity.
-     *
-     * @return the quantity
-     */
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    /**
-     * Sets quantity.
-     *
-     * @param quantity the quantity
-     */
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    /**
-     * Gets total price.
-     *
-     * @return the total price
-     */
-    public String getTotalPrice() {
-        return totalPrice;
-    }
-
-    /**
-     * Sets total price.
-     *
-     * @param totalPrice the total price
-     */
-    public void setTotalPrice(String totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    /**
-     * Gets product.
-     *
-     * @return the product
-     */
-    public Product getProduct() {
-        return product;
-    }
-
-    /**
-     * Sets product.
-     *
-     * @param product the product
-     */
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    /**
-     * Gets sale.
-     *
-     * @return the sale
-     */
-    public Sale getSale() {
-        return sale;
-    }
-
-    /**
-     * Sets sale.
-     *
-     * @param sale the sale
-     */
-    public void setSale(Sale sale) {
-        this.sale = sale;
-    }
-
-    /**
-     * Gets sale refunded product.
-     *
-     * @return the sale refunded product
-     */
-    public SaleRefundedProduct getSaleRefundedProduct() {
-        return saleRefundedProduct;
-    }
-
-    /**
-     * Sets sale refunded product.
-     *
-     * @param saleRefundedProduct the sale refunded product
-     */
-    public void setSaleRefundedProduct(SaleRefundedProduct saleRefundedProduct) {
-        this.saleRefundedProduct = saleRefundedProduct;
-    }
 }
